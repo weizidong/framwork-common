59633f15fe162a14f5725a222855461e2aec8b9e
魏自东 <277195991@qq.com>
2017-10-23 17:48:24 +0800

    ...

--- src/main/java/org/wzd/framwork/utils/CookieUtil.java
+++ src/main/java/org/wzd/framwork/utils/CookieUtil.java
@@ -27,9 +27,7 @@ public class CookieUtil {
 	public static void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
 		Cookie cookie = new Cookie(name, value);
 		cookie.setPath("/");
-		if (maxAge > 0) {
-			cookie.setMaxAge(maxAge);
-		}
+		cookie.setMaxAge(maxAge);
 		response.addCookie(cookie);
 	}
 
