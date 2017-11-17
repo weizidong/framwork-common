470c66b3026b5ae8a36c931acaa901340f892a61
魏自东 <277195991@qq.com>
2017-11-17 17:49:18 +0800

    ...

--- src/main/java/org/wzd/framwork/utils/AESUtil.java
+++ src/main/java/org/wzd/framwork/utils/AESUtil.java
@@ -51,10 +51,12 @@ public class AESUtil {
 	}
 
 	/**
-	 * 解密 解密过程： 1.同加密1-4步 2.将加密后的字符串反纺成byte[]数组 3.将加密内容解密
+	 * 加密 1.构造密钥生成器 2.根据secretKey规则初始化密钥生成器 3.产生密钥 4.创建和初始化密码器 5.内容加密 6.返回字符串
 	 * 
+	 * @param secretKey
+	 *            密匙
 	 * @param content
-	 *            解密内容
+	 *            加密内容
 	 * @return
 	 */
 	public String AESEncode(String content) {
